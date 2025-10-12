e233268740ef3f6401ae406056702bcd1eb00963
Sadeq Eslah <sadeqeslah@gmail.com>
2025-10-12 20:53:14 +0330

    version 1.0

---
+++ src/swiper.js
@@ -0,0 +1,63 @@
+// گالری اول معرفی برترین محصولات
+var swiper = new Swiper(".mySwiper1", {
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: false,
+  },
+});
+
+// category-banner-1
+var swiper = new Swiper(".mySwiper2", {
+  slidesPerView: 3.5,
+  spaceBetween: 30,
+});
+// استوری دایره ای
+var swiper = new Swiper(".mySwiper2-lg", {
+  slidesPerView: 8,
+  spaceBetween: 10,
+});
+// category-banner-3
+var swiper = new Swiper(".mySwiper2-md", {
+  slidesPerView: 4.5,
+  spaceBetween: 10,
+});
+// شگفت انگیز موبایل
+var swiper = new Swiper(".mySwiper3", {
+  slidesPerView: 2.5,
+  spaceBetween: 10,
+  loop: true,
+});
+// شگفت انگیز لب تاب
+var swiper = new Swiper(".mySwiper3-lg", {
+  slidesPerView: 6.5,
+  spaceBetween: 10,
+  loop: true,
+});
+// شگفت انگیز تبلت
+var swiper = new Swiper(".mySwiper3-md", {
+  slidesPerView: 3.5,
+  spaceBetween: 5,
+  loop: true,
+});
+
+// برند عطر - برند ژرورا موبایل
+var swiper = new Swiper(".mySwiper4", {
+  slidesPerView: 2.5,
+  spaceBetween: 10,
+  loop: true,
+});
+// برند عطر - برند ژرورا لب تاب
+var swiper = new Swiper(".mySwiper4-lg", {
+  slidesPerView: 5,
+  spaceBetween: 10,
+  loop: true,
+});
+// برند عطر - برند ژرورا تبلت
+var swiper = new Swiper(".mySwiper4-md", {
+  slidesPerView: 3.5,
+  spaceBetween: 5,
+  loop: true,
+});
